582a540ce97b2e4af056328e1c184a56c572f5b1
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-27 18:54:15 +0400

    chore: unneeded `@ComponentScan` removed

--- src/main/java/com/itjobmarketanalytics/webui/config/Config.java
+++ src/main/java/com/itjobmarketanalytics/webui/config/Config.java
@@ -2,15 +2,12 @@ package com.itjobmarketanalytics.webui.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-@ComponentScan("com.itjobmarketanalytics.webui")
 public class Config {
-
     @Bean
     RestTemplate restTemplate () {
         RestTemplate restTemplate = new RestTemplate();
