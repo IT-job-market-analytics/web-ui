ca85245f63eff70674c5581d05d1d14f8afe8ebc
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-27 18:53:52 +0400

    chore: no args constructors for DTO
    Allows `restTemplate.exchange` to return instances of Dto, instead of strings.

--- src/main/java/com/itjobmarketanalytics/webui/dto/SignInResponseDto.java
+++ src/main/java/com/itjobmarketanalytics/webui/dto/SignInResponseDto.java
@@ -2,11 +2,13 @@ package com.itjobmarketanalytics.webui.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class SignInResponseDto {
     Long id;
     String username;
