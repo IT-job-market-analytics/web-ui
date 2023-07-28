1115f6e8f50f8390f1af35dc032797584ed09207
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-07-29 00:09:52 +0300

    feat: logged in and logged out option in header

--- src/main/java/com/itjobmarketanalytics/webui/controller/HomePageController.java
+++ src/main/java/com/itjobmarketanalytics/webui/controller/HomePageController.java
@@ -1,5 +1,6 @@
 package com.itjobmarketanalytics.webui.controller;
 
+import com.itjobmarketanalytics.webui.dto.UserDto;
 import com.itjobmarketanalytics.webui.exception.RestApiException;
 import com.itjobmarketanalytics.webui.service.RestApiClientService;
 import jakarta.servlet.http.HttpSession;
@@ -7,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.HttpServerErrorException;
 
 @Controller
 @Slf4j
@@ -22,11 +24,14 @@ public class HomePageController {
     public String homeView(Model model, HttpSession session) {
         log.info("Access token -> {}", session.getAttribute("accessToken"));
 
-
         try {
-            service.getUser((String) session.getAttribute("accessToken"));
+            UserDto userDto = service.getUser((String) session.getAttribute("accessToken"));
+            log.info("Username -> {}", userDto.getUsername());
+            model.addAttribute("username", userDto.getUsername());
         } catch (RestApiException e) {
-            throw new RuntimeException(e);
+            log.info("I can catch RestApiException");
+        } catch (HttpServerErrorException e) {
+            log.info("I can catch error 500");
         }
 
         return "index";
