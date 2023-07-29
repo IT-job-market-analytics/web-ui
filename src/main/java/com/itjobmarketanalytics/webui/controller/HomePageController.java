204062e72e8bb77c4f95ebb81b14048ffffddce7
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-29 19:25:17 +0400

    refactor: `HomePageController` ignore unauthorized errors by exception type rather than by error message
    Also, always create `isLoggedIn` attribute even when it is `false`.

--- src/main/java/com/itjobmarketanalytics/webui/controller/HomePageController.java
+++ src/main/java/com/itjobmarketanalytics/webui/controller/HomePageController.java
@@ -2,6 +2,7 @@ package com.itjobmarketanalytics.webui.controller;
 
 import com.itjobmarketanalytics.webui.dto.UserDto;
 import com.itjobmarketanalytics.webui.exception.RestApiException;
+import com.itjobmarketanalytics.webui.exception.RestApiUnauthorizedException;
 import com.itjobmarketanalytics.webui.service.RestApiClientService;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 @Slf4j
 public class HomePageController {
-
     private final RestApiClientService service;
 
     public HomePageController(RestApiClientService service) {
@@ -21,17 +21,18 @@ public class HomePageController {
 
     @GetMapping("/")
     public String homeView(Model model, HttpSession session) {
-
+        boolean isLoggedIn = true;
         try {
             UserDto userDto = service.getUser((String) session.getAttribute("accessToken"));
             model.addAttribute("username", userDto.getUsername());
-            model.addAttribute("isLoggedIn", true);
         } catch (RestApiException e) {
-            String errorMessage = e.getMessage();
-            if (!errorMessage.equalsIgnoreCase("Unauthorized")) {
+            if (!(e instanceof RestApiUnauthorizedException)) {
                 model.addAttribute("errorMessage", e.getMessage());
             }
+
+            isLoggedIn = false;
         }
+        model.addAttribute("isLoggedIn", isLoggedIn);
 
         return "index";
     }
