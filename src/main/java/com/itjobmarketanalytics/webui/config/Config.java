73cfeed4a3a2eb27ec9a318b7bae46ff73474d1d
Sergey Zhukov <zhukovsd@gmail.com>
2023-07-29 13:51:29 +0400

    feat: parse 401 response body and determine error message
    Default `RestTemplate` has to be replaced for this to work - https://github.com/spring-attic/spring-security-oauth/issues/441.

--- src/main/java/com/itjobmarketanalytics/webui/config/Config.java
+++ src/main/java/com/itjobmarketanalytics/webui/config/Config.java
@@ -1,20 +1,24 @@
 package com.itjobmarketanalytics.webui.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+import java.util.function.Supplier;
 
 @Configuration
 public class Config {
-    @Bean
-    RestTemplate restTemplate () {
-        RestTemplate restTemplate = new RestTemplate();
-        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
-        converter.setObjectMapper(new ObjectMapper());
-        restTemplate.getMessageConverters().add(converter);
+    public static class CustomHttpRequestFactory implements Supplier<ClientHttpRequestFactory> {
+        @Override
+        public ClientHttpRequestFactory get() {
+            return new HttpComponentsClientHttpRequestFactory();
+        }
+    }
 
-        return restTemplate;
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return restTemplateBuilder.requestFactory(new CustomHttpRequestFactory()).build();
     }
 }
