b6c5d568f5b8618418fdf4bcb234120a2bb5dd74
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-07-31 20:46:15 +0300

    feat: added message for user to show successful or failed form request
    ref: delete unused object

---
+++ src/main/java/com/itjobmarketanalytics/webui/controller/ProfileController.java
@@ -0,0 +1,125 @@
+package com.itjobmarketanalytics.webui.controller;
+
+import com.itjobmarketanalytics.webui.dto.UserDto;
+import com.itjobmarketanalytics.webui.dto.UserSubscriptionsDto;
+import com.itjobmarketanalytics.webui.dto.UserUpdateDto;
+import com.itjobmarketanalytics.webui.exception.RestApiException;
+import com.itjobmarketanalytics.webui.exception.RestApiUnauthorizedException;
+import com.itjobmarketanalytics.webui.service.RestApiClientService;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@Slf4j
+public class ProfileController {
+
+    private final RestApiClientService service;
+
+    public ProfileController(RestApiClientService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/profile")
+    public String profileView(Model model, HttpSession session) {
+        boolean isLoggedIn = true;
+        String token = (String) session.getAttribute("accessToken");
+        try {
+            UserDto userDto = service.getUser((String) session.getAttribute("accessToken"));
+            model.addAttribute("username", userDto.getUsername());
+            model.addAttribute("updateUser", new UserUpdateDto());
+
+            List<UserSubscriptionsDto> availableSubscriptions = service.getAvailableSubscriptions(token);
+            model.addAttribute("availableSubscriptions", availableSubscriptions);
+            model.addAttribute("addSubscription", new UserSubscriptionsDto());
+
+            List<UserSubscriptionsDto> currentSubscriptions = service.getCurrentSubscriptions(token);
+            model.addAttribute("currentSubscriptions", currentSubscriptions);
+            model.addAttribute("removeSubscription", new UserSubscriptionsDto());
+
+
+        } catch (RestApiException e) {
+            if (e instanceof RestApiUnauthorizedException) {
+                return "redirect:/sign-in";
+            }
+            model.addAttribute("errorMessage", e.getMessage());
+        }
+        model.addAttribute("isLoggedIn", isLoggedIn);
+
+        return "profile";
+    }
+
+    @PostMapping("/profile/editUser")
+    public String updateUserInForm(
+            @ModelAttribute("updateUser")   UserUpdateDto userUpdateDto,
+                                            Model model,
+                                            HttpSession session,
+                                            RedirectAttributes redirectAttributes) {
+
+        try {
+            UserDto userDto = service.updateUser(userUpdateDto.getTelegramChatId(), (String)session.getAttribute("accessToken"));
+            redirectAttributes.addFlashAttribute("updateMessage", "Successfully updated");
+            redirectAttributes.addFlashAttribute("updateAlertClass", "alert-success");
+        } catch (RestApiException e) {
+            if (e instanceof RestApiUnauthorizedException) {
+                return "redirect:/sign-in";
+            }
+            redirectAttributes.addFlashAttribute("updateMessage", e.getMessage());
+            redirectAttributes.addFlashAttribute("updateAlertClass", "alert-danger");
+            return "redirect:/profile";
+        }
+
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/profile/addSubscription")
+    public String addSubscription(
+            @ModelAttribute("addSubscription")  UserSubscriptionsDto addSubscription,
+                                                HttpSession session,
+                                                RedirectAttributes redirectAttributes) {
+        try {
+            service.addSubscriptions(addSubscription.getQuery(), (String) session.getAttribute("accessToken"));
+            redirectAttributes.addFlashAttribute("addMessage", "Successfully added");
+            redirectAttributes.addFlashAttribute("addAlertClass", "alert-success");
+        } catch (RestApiException e) {
+            if (e instanceof RestApiUnauthorizedException) {
+                return "redirect:/sign-in";
+            }
+            redirectAttributes.addFlashAttribute("addMessage", e.getMessage());
+            redirectAttributes.addFlashAttribute("addAlertClass", "alert-danger");
+            return "redirect:/profile";
+        }
+
+        return "redirect:/profile";
+    }
+
+    @PostMapping("/profile/removeSubscription")
+    public String removeSubscription(
+            @ModelAttribute("removeSubscription")   UserSubscriptionsDto removeSubscription,
+                                                    Model model,
+                                                    HttpSession session,
+                                                    RedirectAttributes redirectAttributes) {
+        try {
+            service.removeSubscriptions(removeSubscription.getQuery(), (String) session.getAttribute("accessToken"));
+            redirectAttributes.addFlashAttribute("removeMessage", "Successfully removed");
+            redirectAttributes.addFlashAttribute("removeAlertClass", "alert-success");
+
+        } catch (RestApiException e) {
+            if (e instanceof RestApiUnauthorizedException) {
+                return "redirect:/sign-in";
+            }
+            redirectAttributes.addFlashAttribute("removeMessage", e.getMessage());
+            redirectAttributes.addFlashAttribute("removeAlertClass", "alert-danger");
+            return "redirect:/profile";
+        }
+
+        return "redirect:/profile";
+    }
+}
--- src/main/java/com/itjobmarketanalytics/webui/controller/TestController.java
+++ src/main/java/com/itjobmarketanalytics/webui/controller/TestController.java
@@ -6,11 +6,13 @@ import com.itjobmarketanalytics.webui.dto.SignUpDto;
 import com.itjobmarketanalytics.webui.dto.UserDto;
 import com.itjobmarketanalytics.webui.exception.RestApiException;
 import com.itjobmarketanalytics.webui.service.RestApiClientService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
@@ -39,5 +41,49 @@ public class TestController {
     @GetMapping(value = {"/user"})
     public UserDto getUser(@RequestParam String token) throws RestApiException {
         return restApiClientService.getUser(token);
+    }
+
+    @GetMapping(value = {"/profile"})
+    public void updateUser(
+            @RequestParam Long telegramChatId,
+            @RequestParam String token
+    ) throws RestApiException {
+        log.info("Test: updateUser -> start");
+        restApiClientService.updateUser(telegramChatId, token);
+        log.info("Test: updateUser -> success");
+    }
+
+    @GetMapping(value = {"/subscriptions"})
+    public void getCurrentSubscription(@RequestParam String token) throws RestApiException {
+        log.info("Test: get subscriptions -> start");
+        restApiClientService.getCurrentSubscriptions(token);
+        log.info("Test: get subscriptions -> success");
+    }
+
+    @GetMapping(value = {"/subscriptions/allAvailable"})
+    public void getAllAvailableSubscriptions(@RequestParam String token) throws RestApiException {
+        log.info("Test: get all available subscriptions -> start");
+        restApiClientService.getAvailableSubscriptions(token);
+        log.info("Test: get all available subscriptions -> success");
+    }
+
+    @GetMapping(value = {"/subscriptions/add"})
+    public void addSubscriptions(
+            @RequestParam String query,
+            @RequestParam String token
+            ) throws RestApiException {
+        log.info("Test: add subscriptions -> start");
+        restApiClientService.addSubscriptions(query, token);
+        log.info("Test: add subscriptions -> success");
+    }
+
+    @GetMapping(value = {"/subscriptions/delete"})
+    public void deleteSubscriptions(
+            @RequestParam String query,
+            @RequestParam String token
+    ) throws RestApiException {
+        log.info("Test: delete subscriptions -> start");
+        restApiClientService.removeSubscriptions(query, token);
+        log.info("Test: delete subscriptions -> success");
     }
 }
